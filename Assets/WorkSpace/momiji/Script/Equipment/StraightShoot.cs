e58b03f40dcdfc52b9cbbdc51e9a6e6ff1605e3c
momiji1107 <yokohirahazuki0607@gmail.com>
2026-04-12 14:22:40 +0900

    skillとStraightShootの調整
    実行前にSkillを設定できるようにした。
    StraightShootの弾を画面外まで飛ばすかどうか設定できるようにした。

--- Assets/WorkSpace/momiji/Script/Equipment/StraightShoot.cs
+++ Assets/WorkSpace/momiji/Script/Equipment/StraightShoot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class StraightShoot : EquipmentBase
 {
     [SerializeField] private GameObject bulletPrefab;
+    public bool flyEndless = false;
 
     public StraightShoot()
     {
@@ -15,6 +16,8 @@ public class StraightShoot : EquipmentBase
         GameObject bullet = Instantiate(bulletPrefab, model.Player.transform.position, Quaternion.identity);
         bullet.GetComponent<BulletBase>()?.SetDamage(model.Attack);
         bullet.GetComponent<Rigidbody2D>().linearVelocity = model.Player.transform.right * model.ShootSpeed;
-        Destroy(bullet,2f);
+        
+        //flyEndless = falseだったら2秒後にこのオブジェクトを削除
+        if(!flyEndless) Destroy(bullet,2f);
     }
 }
