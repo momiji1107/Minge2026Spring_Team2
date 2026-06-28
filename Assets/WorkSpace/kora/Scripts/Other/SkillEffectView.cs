72a1c23da02da2b497450d994aaa451f77118a70
Kora31 <168075070+Kora31@users.noreply.github.com>
2026-06-28 15:04:01 +0900

    バリア消滅アニメーションを追加し、ダメージ軽減スキルを削除
    - バリア消滅アニメーションを追加した
    - DamageReductionスキルのスクリプト、SOを削除し、DamageNegateが直接表示されるように変更した

--- Assets/WorkSpace/kora/Scripts/Other/SkillEffectView.cs
+++ Assets/WorkSpace/kora/Scripts/Other/SkillEffectView.cs
@@ -3,6 +3,16 @@ using UnityEngine;
 public class SkillEffectView : MonoBehaviour
 {
     private Animator _animator;
+    private bool _isActivated = false;
+    private float _duration = 0f;
+    private float _timer = 0f;
+    private State _state = State.Activate;
+
+    private enum State
+    {
+        Activate,
+        DisActivate
+    }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,10 +26,46 @@ public class SkillEffectView : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var info = _animator.GetCurrentAnimatorStateInfo(0);
-        if (info.normalizedTime >= 1)
+        if (!_isActivated)
         {
-            Destroy(gameObject);
+            var info = _animator.GetCurrentAnimatorStateInfo(0);
+            if (info.normalizedTime >= 1)
+            {
+                Destroy(gameObject);
+            }
         }
+        else
+        {
+            switch (_state)
+            {
+                case State.Activate:
+                    _timer += Time.deltaTime;
+                    if (_timer <= _duration) return;
+                    
+                    _state = State.DisActivate;
+                    _animator.SetTrigger("OnDisActive");
+                    break;
+                
+                case State.DisActivate:
+                    var info = _animator.GetCurrentAnimatorStateInfo(0);
+                    if (info.normalizedTime >= 1)
+                    {
+                        Destroy(gameObject);
+                    }
+                    break;
+            }
+        }
+
+    }
+
+    private void NextState()
+    {
+        if(!_isActivated) Destroy(gameObject);
+    }
+
+    public void Activate(float duration)
+    {
+        _isActivated = true;
+        _duration = duration;
     }
 }
