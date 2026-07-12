dde09bc1ea85aa5fee7dfa926a21df53c9b4dcfa
momiji1107 <yokohirahazuki0607@gmail.com>
2026-07-12 17:10:03 +0900

    通常攻撃のクールタイム短縮アップグレードを追加した

--- Assets/WorkSpace/momiji/Script/Upgrade/RapidFireSpeedUpgrade.cs
+++ Assets/WorkSpace/momiji/Script/Upgrade/RapidFireSpeedUpgrade.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 public class RapidFireSpeedUpgrade : UpgradeBase
 {
     [SerializeField] private float rapidFireSpeed;
+    
+    void OnEnable()
+    {
+        titleName = "クールタイム短縮";
+        infoSentence = $"通常こうげきの\nクールタイムが\n{rapidFireSpeed}s 早くなる";
+    }
+    
     public override bool CanAppear(PlayerEquipmentManager equipmentManager)
     {
-        if(equipmentManager.Model.RapidFireSpeed > rapidFireSpeed) return true;
+        if(PlayerSelection.selectedCharacter == CharacterName.PLAYER_TWO) return false;
+        else if(equipmentManager.Model.RapidFireSpeed > rapidFireSpeed) return true;
         return false;
     }
 
